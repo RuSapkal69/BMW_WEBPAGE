4b1deef35020a930881ea995fd8b4f5514f0f3f5
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-02-22 23:49:51 +0530

    Video Carousel fixed

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -53,53 +53,70 @@ const VideoCarousel = () => {
     useEffect(() => {
         let currentProgress = 0;
         let span = videoSpanRef.current;
-        let animUpdate = () => {};
+    
         if (span[videoId]) {
-            //animation the video progress bar
-            let anim = gsap.to(span[videoId], {
-                onUpdate: () => {
-                    const progress = Math.ceil(anim.progress() * 100);
-                    if(progress != currentProgress) {
-                        currentProgress = progress;
-
-                        gsap.to(videoDivRef.current[videoId], {
-                            width: window.innerWidth < 760 ? '10vw' : window.innerWidth < 1200 ? '10vw' : '4vw'
-                        })
-
-                        gsap.to(span[videoId], {
-                            width: `${currentProgress}%`,
-                            backgroundColor: 'white'
-                        })
-                    }
-                },
-                onComplete: () => {
-                    if(isPlaying) {
-                        gsap.to(videoDivRef.current[videoId], {
-                            width: '12px'
-                        })
-
-                        gsap.to(span[videoId], {
-                            backgroundColor: '#afafaf'
-                        })
-                    }
-                },
-            })
-
-            if (videoId === 0) {
-                anim.restart();
-            }
-            const animUpdate = () => {
-                anim.progress(videoRef.current[videoId] / hightlightsSlides[videoId].videoDuration);
-            }
-        }
-
-        if(isPlaying) {
+          // animation to move the indicator
+          let anim = gsap.to(span[videoId], {
+            onUpdate: () => {
+              // get the progress of the video
+              const progress = Math.ceil(anim.progress() * 100);
+    
+              if (progress != currentProgress) {
+                currentProgress = progress;
+    
+                // set the width of the progress bar
+                gsap.to(videoDivRef.current[videoId], {
+                  width:
+                    window.innerWidth < 760
+                      ? "10vw" // mobile
+                      : window.innerWidth < 1200
+                      ? "10vw" // tablet
+                      : "4vw", // laptop
+                });
+    
+                // set the background color of the progress bar
+                gsap.to(span[videoId], {
+                  width: `${currentProgress}%`,
+                  backgroundColor: "white",
+                });
+              }
+            },
+    
+            // when the video is ended, replace the progress bar with the indicator and change the background color
+            onComplete: () => {
+              if (isPlaying) {
+                gsap.to(videoDivRef.current[videoId], {
+                  width: "12px",
+                });
+                gsap.to(span[videoId], {
+                  backgroundColor: "#afafaf",
+                });
+              }
+            },
+          });
+    
+          if (videoId == 0) {
+            anim.restart();
+          }
+    
+          // update the progress bar
+          const animUpdate = () => {
+            anim.progress(
+              videoRef.current[videoId].currentTime /
+                hightlightsSlides[videoId].videoDuration
+            );
+          };
+    
+          if (isPlaying) {
+            // ticker to update the progress bar
             gsap.ticker.add(animUpdate);
-        } else {
+          } else {
+            // remove the ticker when the video is paused (progress bar is stopped)
             gsap.ticker.remove(animUpdate);
+          }
         }
-
-    }, [videoId, startPlay])
+      }, [videoId, startPlay]);
+    
 
     const handleProcess = (type, index) => {
         switch (type) {
